b714848991a5a50475ed6bd0fc09ea43fd792373
feihu <378917466@qq.com>
2018-06-22 22:22:37 +0800

    当月不返积分

--- VIPExtend/ApplicationInstance.cs
+++ VIPExtend/ApplicationInstance.cs
@@ -62,11 +62,19 @@ namespace VIPExtend
 
                     foreach (var vipinfo in vipInfos)
                     {
+                        if (vipinfo.vip_start_date.HasValue && vipinfo.vip_start_date.Value.Month == DateTime.Now.Month)
+                        {//当月不反积分
+                            continue;
+                        }
+
                         var accnum = int.Parse(System.Configuration.ConfigurationManager.AppSettings["VipAccnum"]); ;
                         if (db.t_rm_vip_acclist.Any(o => o.oper_des == "活动##送积分" && o.memo == "活动##送积分" && o.card_no == vipinfo.card_no && o.acc_num == accnum && o.oper_id == "1001" && o.ope_date.Value.Month == DateTime.Now.Month && o.ope_date.Value.Year == DateTime.Now.Year))
                         {//如果这个月份已经送过积分 则跳过
                             continue;
                         }
+
+
+
 
                         if (ignoreList.Contains(vipinfo.card_no))
                         {//如果会员忽略列表里存在这个会员卡号，择跳过
